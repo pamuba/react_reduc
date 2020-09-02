5a2a3d7eaf0bad2fcffb3dbc810159d2c235f99c
pamuba <pamuba@users.noreply.github.com>
2020-09-02 19:45:30 +0530

    1

--- App.js
+++ App.js
@@ -2,14 +2,32 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+//Redux 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import allReducers from './reducers';
+import TaskManagerComponent from './components/TaskManagerComponent';
+
+
+let store = createStore(allReducers);
+const App = () => (
+    <Provider store={store}>
+      <View style={styles.container}>
+         <TaskManagerComponent />
+      </View>
+    </Provider>
+);
+
+export default App;
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
